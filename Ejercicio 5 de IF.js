4da53e5853d259bf089f01bd485097b65a66dc28
Josue Chiguichon <josueperezw21132@gmail.com>
2025-02-15 20:09:55 -0600

    Ejercicio De Estructura De If

---
+++ Ejercicio 5 de IF.js
@@ -0,0 +1,15 @@
+/*UN CINE OFRECE DIFERENTES PRECIOS DE ENTRADA SEGÚN LA EDAD DE LOS ESPECTADORES. LOS MENORES DE 12 AÑOS PAGAN 5 DÓLARES, LOS MAYORES DE 65 AÑOS PAGAN 7 DÓLARES, Y LOS DEMÁS PAGAN 10 DÓLARES. ESCRIBE UN PROGRAMA QUE PREGUNTE LA EDAD DEL ESPECTADOR Y CALCULE EL PRECIO DE ENTRADA.*/
+let edad = prompt("Ingrese su edad:")
+edad = parseInt(edad)
+
+let precio
+if (edad < 12) {
+    precio = 5
+} else if (edad > 65) {
+    precio = 7
+} else {
+    precio = 10
+}
+
+console.log(`El precio de la entrada es: $${precio}`)
+alert(`El precio de la entrada es: $${precio}`)
